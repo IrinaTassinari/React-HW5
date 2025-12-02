a220854c354a8dc7cd7e9643157b569a9e036a93
IrinaTassinari <irinatassinari@gmail.com>
2025-12-02 01:51:08 +0100

    changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,19 +5,28 @@ import XIcon from '../../assets/icons/x-app.svg'
 import AppleIconHover from '../../assets/icons/apple-store-hover.svg'
 import GoogleIconHover from '../../assets/icons/google-app-hover.svg'
 import XIconHover from '../../assets/icons/x-app-hover.svg'
+import { useState } from "react";
 
 
 function Footer() {
-    function iconHover(){
-        
+
+  const [apple,setApple] = useState(AppleIcon)
+  const [google,setGoogle] = useState(GoogleIcon)
+  const [x,setX] = useState(XIcon)
+
+    function iconHover(setter,hoverSrc){
+        setter(hoverSrc)
+    }
+    function iconLeave(setter,defaultSrc){
+      setter(defaultSrc)
     }
 
   return (
     <div className={style.icons_wrapper}>
       <div className={style.icons_frame}>
-        <a href="#"><img onMouseOver={iconHover} className={style.icon} src={AppleIcon} alt="apple-store" /> </a>
-        <a href="#"><img onMouseOver={iconHover} className={style.icon} src={GoogleIcon} alt="google-app" /></a>
-        <a href="#"><img onMouseOver={iconHover} className={style.icon} src={XIcon} alt="x-app" /></a>
+        <a href="#"><img  className={style.icon} src={apple} alt="apple-store" onMouseOver={() => iconHover(setApple, AppleIconHover)}  onMouseOut={() => iconLeave(setApple, AppleIcon)} /> </a>
+        <a href="#"><img className={style.icon} src={google} alt="google-app"  onMouseOver={() => iconHover(setGoogle, GoogleIconHover)} onMouseOut={() => iconLeave(setGoogle, GoogleIcon)}/></a>
+        <a href="#"><img className={style.icon} src={x} alt="x-app"  onMouseOver={() => iconHover(setX, XIconHover)} onMouseOut={() => iconLeave(setX, XIcon)} /></a>
       </div>
     </div>
   );
